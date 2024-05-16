07dc8e8e97c888927bc4dcdccd7326eab097f851
Nipunkhattri <nipunkhattri321@gmail.com>
2024-05-16 17:25:48 +0100

    done Emotion detection using deep learning and open cv

---
+++ face_detection.py
@@ -0,0 +1,77 @@
+import cv2
+from keras.models import model_from_json
+import numpy as np
+
+# Load the pre-trained model architecture from JSON file
+json_file = open("emotion_detection_model.json", "r")
+model_json = json_file.read()
+json_file.close()
+model = model_from_json(model_json)
+
+# Load the pre-trained model weights
+model.load_weights("emotion_detection_model.h5")
+
+# Load the Haar cascade classifier for face detection
+haar_file = 'haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(haar_file)
+
+# Define a function to extract features from an image
+def extract_features(image):
+    feature = np.array(image)
+    feature = feature.reshape(1, 48, 48, 1)
+    return feature / 255.0
+
+# Open the webcam (camera)
+webcam = cv2.VideoCapture(0)
+
+# Define labels for emotion classes
+labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
+
+while True:
+    # Read a frame from the webcam
+    i, im = webcam.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the grayscale frame
+    faces = face_cascade.detectMultiScale(im, 1.3, 5)
+
+    try:
+        # For each detected face, perform facial emotion recognition
+        for (p, q, r, s) in faces:
+            # Extract the region of interest (ROI) which contains the face
+            image = gray[q:q + s, p:p + r]
+
+            # Draw a rectangle around the detected face
+            cv2.rectangle(im, (p, q), (p + r, q + s), (255, 0, 0), 2)
+
+            # Resize the face image to the required input size (48x48)
+            image = cv2.resize(image, (48, 48))
+
+            # Extract features from the resized face image
+            img = extract_features(image)
+
+            # Make a prediction using the trained model
+            pred = model.predict(img)
+
+            # Get the predicted label for emotion
+            prediction_label = labels[pred.argmax()]
+
+            # Display the predicted emotion label near the detected face
+            cv2.putText(im, f'Emotion: {prediction_label}', (p - 10, q - 10),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255))
+
+        # Display the frame with annotations in real-time
+        cv2.imshow("Real-time Facial Emotion Recognition", im)
+
+        # Break the loop if the 'Esc' key is pressed
+        if cv2.waitKey(1) == 27:
+            break
+
+    except cv2.error:
+        pass
+
+# Release the webcam and close all OpenCV windows
+webcam.release()
+cv2.destroyAllWindows()
